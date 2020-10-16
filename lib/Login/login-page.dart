cb09fe34740de2126b15a75edcfd1063545c1e20
Pedro Henrique Gonçalves Bonel <pedro.bonel@gmail.com>
2020-10-15 21:51:04 -0300

    Tentando fazer essa bosta rodar

--- lib/Login/login-page.dart
+++ lib/Login/login-page.dart
@@ -1,4 +1,4 @@
-import 'package:lib/Cadastro/cadastro.dart';
+import 'package:blidroid/Cadastro/cadastro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
